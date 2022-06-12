8ee2fd61511fd767a21d04d210395e0b1b2749ef
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-06-12 11:30:07 +0300

    commit tree e7e5962ca676dedda4f875d705be53b63f4b6bd6

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+  ];
+  
+  function superbowlWin(array) {
+    for (const object of array) {
+      if (object.result === "W") {
+        return object.year;
+      }
+    }
+  }
+  record.find(superbowlWin);
+  
